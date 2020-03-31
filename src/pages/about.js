3989928f20e28fe89c6756a66d4b7d6c4210b9c6
Sergey Onufrienko <sergey@onufrienko.com>
2020-04-01 00:27:51 +0100

    Load page content from Contentful

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,33 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const AboutPage = () => (
-  <Layout>
-    <SEO title="" />
-    About content
-  </Layout>
-)
+const pageQuery = graphql`
+  {
+    contentfulPage(slug: { eq: "about" }) {
+      pageTitle
+      text {
+        childMarkdownRemark {
+          html
+        }
+      }
+      metaDescription
+    }
+  }
+`
+
+const AboutPage = () => {
+  const { contentfulPage } = useStaticQuery(pageQuery)
+  const { pageTitle, metaDescription, text } = contentfulPage
+  const html = text.childMarkdownRemark.html
+
+  return (
+    <Layout>
+      <SEO title={pageTitle} description={metaDescription} />
+      <div className="pageContent" dangerouslySetInnerHTML={{ __html: html }} />
+    </Layout>
+  )
+}
 
 export default AboutPage
